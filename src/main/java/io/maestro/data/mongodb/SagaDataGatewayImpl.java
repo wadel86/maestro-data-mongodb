8454462db78d31b1a61cee573b542ddc78f88e15
Adel WERGHI <adel.werghi.prestataire@sfr.com>
2021-11-16 18:04:33 +0100

    Implement save and find saga methods

--- src/main/java/io/maestro/data/mongodb/SagaDataGatewayImpl.java
+++ src/main/java/io/maestro/data/mongodb/SagaDataGatewayImpl.java
@@ -1,21 +1,32 @@
 package io.maestro.data.mongodb;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import io.maestro.common.command.CommandWithDestination;
 import io.maestro.common.port.SagaDataGateway;
 import io.maestro.common.saga.instance.SagaInstance;
+import org.bson.Document;
 
 public class SagaDataGatewayImpl implements SagaDataGateway {
 
     private final MongoClient mongoClient;
+    private final MongoDatabase mongoDatabase;
+    private final MongoCollection<SagaInstance> sagaCollection;
+    private final MongoCollection<CommandWithDestination> eventsCollection;
 
     public SagaDataGatewayImpl(MongoClient mongoClient) {
         this.mongoClient = mongoClient;
+        this.mongoDatabase = this.mongoClient.getDatabase("saga_db");
+        this.sagaCollection = this.mongoDatabase.getCollection("saga", SagaInstance.class);
+        this.eventsCollection = this.mongoDatabase.getCollection("events", CommandWithDestination.class);
     }
 
     @Override
     public SagaInstance saveSaga(SagaInstance saga) {
-        return null;
+        //set the id here.
+        this.sagaCollection.insertOne(saga);
+        return saga;
     }
 
     @Override
@@ -25,6 +36,7 @@ public class SagaDataGatewayImpl implements SagaDataGateway {
 
     @Override
     public SagaInstance findSaga(String sagaId, String sagaType) {
-        return null;
+        return this.sagaCollection.find(new Document("id", sagaId), SagaInstance.class)
+                                  .first();
     }
 }
